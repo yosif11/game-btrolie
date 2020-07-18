48f679591d7d86090e7faa90788a482e564bb030
yosif11 <49305870+yosif11@users.noreply.github.com>
2020-07-19 02:17:10 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,244 @@
+const express = require("express");
+const app = express();
+
+app.listen(() => console.log("start btrolie"));
+
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const cmd = require("node-cmd");
+const ms = require("ms");
+const fs = require('fs');
+const ytdl = require("ytdl-core");
+const canvas = require("canvas");
+const convert = require("hh-mm-ss")
+const fetchVideoInfo = require("youtube-info");
+const simpleytapi = require('simple-youtube-api')
+const util = require("util")
+const gif = require("gif-search");
+const jimp = require("jimp");
+const guild = require('guild');
+const hastebins = require('hastebin-gen');
+const getYoutubeID = require('get-youtube-id');
+const pretty = require("pretty-ms");
+const moment = require('moment');
+const request = require('request');
+const dateFormat = require('dateformat');
+
+//لا تلعب اي شي في الكود
+
+
+
+const prefix = ""
+const developers = ""
+
+////////
+
+
+
+
+
+
+
+
+const cuttweets = [
+  'كت تويت ‏| تخيّل لو أنك سترسم شيء وحيد فيصبح حقيقة، ماذا سترسم؟',
+  'كت تويت | أكثر شيء يُسكِت الطفل برأيك؟',
+  'كت تويت | الحرية لـ ... ؟',
+  'كت تويت | قناة الكرتون المفضلة في طفولتك؟',
+  'كت تويت ‏| كلمة للصُداع؟',
+  'كت تويت ‏| ما الشيء الذي يُفارقك؟',
+  'كت تويت | موقف مميز فعلته مع شخص ولا يزال يذكره لك؟',
+  'كت تويت ‏| أيهما ينتصر، الكبرياء أم الحب؟',
+  'كت تويت | بعد ١٠ سنين ايش بتكون ؟',
+  'كت تويت ‏| مِن أغرب وأجمل الأسماء التي مرت عليك؟',
+  '‏كت تويت | عمرك شلت مصيبة عن شخص برغبتك ؟',
+  'كت تويت | أكثر سؤال وجِّه إليك مؤخرًا؟',
+  '‏كت تويت | ما هو الشيء الذي يجعلك تشعر بالخوف؟',
+  '‏كت تويت | وش يفسد الصداقة؟',
+  '‏كت تويت | شخص لاترفض له طلبا ؟',
+  '‏كت تويت | كم مره خسرت شخص تحبه؟.',
+  '‏كت تويت | كيف تتعامل مع الاشخاص السلبيين ؟',
+  '‏كت تويت | كلمة تشعر بالخجل اذا قيلت لك؟',
+  '‏كت تويت | جسمك اكبر من عٌمرك او العكسّ ؟!',
+  '‏كت تويت |أقوى كذبة مشت عليك ؟',
+  '‏كت تويت | تتأثر بدموع شخص يبكي قدامك قبل تعرف السبب ؟',
+  'كت تويت | هل حدث وضحيت من أجل شخصٍ أحببت؟',
+  '‏كت تويت | أكثر تطبيق تستخدمه مؤخرًا؟',
+  '‏كت تويت | ‏اكثر شي يرضيك اذا زعلت بدون تفكير ؟',
+  '‏كت تويت | وش محتاج عشان تكون مبسوط ؟',
+  '‏كت تويت | مطلبك الوحيد الحين ؟',
+  '‏كت تويت | هل حدث وشعرت بأنك ارتكبت أحد الذنوب أثناء الصيام؟',
+  'اش رئيك بسيرفرنا'
+]
+ 
+client.on ('message', async (toxicc) => {
+  if (!toxicc.guild || toxicc.author.bot) return false;
+  var prefix = "البرفكس";
+  switch (toxicc.content.split(' ') [0]){
+  case prefix + 'cut':  
+	 
+      var embed = new Discord.RichEmbed().setTitle("Cut Tweet")
+      .setDescription(cuttweets [Math.floor (Math.random () * cuttweets.length)])
+      .setFooter(toxicc.author.tag, toxicc.author.displayAvatarURL)
+      toxicc.channel.send (embed);
+      break;
+  }
+})
+
+
+
+
+var viper = ["https://f.top4top.net/p_682it2tg6.png%22","https://e.top4top.net/p_682a1cus5.png%22","https://d.top4top.net/p_682pycol4.png%22","https://c.top4top.net/p_682vqehy3.png%22","https://b.top4top.net/p_682mlf9d2.png%22","https://a.top4top.net/p_6827dule1.png%22","https://b.top4top.net/p_682g1meb10.png%22","https://a.top4top.net/p_682jgp4v9.png%22","https://f.top4top.net/p_682d4joq8.png%22","https://e.top4top.net/p_6828o0e47.png%22","https://d.top4top.net/p_6824x7sy6.png%22","https://c.top4top.net/p_682gzo2l5.png%22","https://b.top4top.net/p_68295qg04.png%22","https://a.top4top.net/p_682zrz6h3.png%22","https://f.top4top.net/p_6828vkzc2.png%22","https://e.top4top.net/p_682i8tb11.png",]
+    client.on('message', message => {
+        var args = message.content.split(" ").slice(1);
+    if(message.content.startsWith(prefix + 'لوخيروك')) {
+      if(!message.channel.guild) return message.reply('** ممنوع كتابة الاوامر في خاص البوت**');
+         var lo = new Discord.RichEmbed()
+.setImage(viper[Math.floor(Math.random() * viper.length)])
+message.channel.sendEmbed(lo);
+    }
+});
+
+
+
+var memes =["https://cdn.discordapp.com/attachments/422454766468202511/426482204835708938/fkk1.png","https://cdn.discordapp.com/attachments/422454766468202511/426482399757467648/fkk2.png","https://cdn.discordapp.com/attachments/422454766468202511/426486610729762816/fkk3.png","https://cdn.discordapp.com/attachments/422454766468202511/426486726907789332/fkk4.png","https://cdn.discordapp.com/attachments/422454766468202511/426486904343625729/fkk5.png","https://cdn.discordapp.com/attachments/422454766468202511/426487095943495703/fkk6.png","https://cdn.discordapp.com/attachments/422454766468202511/426487229796450304/fkk7.png","https://cdn.discordapp.com/attachments/422454766468202511/426487556461559819/fkk8.png","https://cdn.discordapp.com/attachments/422454766468202511/426487784736423956/fkk9.png","https://cdn.discordapp.com/attachments/422454766468202511/426487945554558991/fkk10.png","https://cdn.discordapp.com/attachments/422454766468202511/426488069672140800/fkk11.png","https://cdn.discordapp.com/attachments/422454766468202511/426488213872312330/fkk12.png","https://cdn.discordapp.com/attachments/422454766468202511/426488358097649688/fkk13.png","https://cdn.discordapp.com/attachments/422454766468202511/426488491359338516/fkk14.png"]
+client.on('message', message => {
+             
+  var prefix = "-";
+if(message.content.startsWith(prefix + 'فكك')) {
+  if(!message.channel.guild) return message.reply('** ممنوع كتابة الاوامر في خاص البوت**');
+     var embed = new Discord.RichEmbed()
+.setImage(memes[Math.floor(Math.random() * memes.length)])
+                .setColor('#7400ff')
+message.channel.sendEmbed(embed);
+}
+});
+
+
+
+var viper = ["https://imgur.com/jxNXIRi.png","","https://imgur.com/bZ77C4N.png","https://imgur.com/JoEkxeB.png","https://imgur.com/ytMmA53.png","https://imgur.com/Tz6leGv.png","https://imgur.com/5cMhvzx.png","https://imgur.com/aP6CJjh.png","https://imgur.com/EZb852E.png","https://imgur.com/kIiz33v.png"]
+    client.on('message', message => {
+        var args = message.content.split(" ").slice(1);
+    if(message.content.startsWith(prefix + 'اسرع')) {
+      if(!message.channel.guild) return message.reply('** ممنوع كتابة الاوامر في خاص البوت**');
+         var lo = new Discord.RichEmbed()
+.setImage(viper[Math.floor(Math.random() * viper.length)])
+message.channel.sendEmbed(lo);
+    }
+});
+
+
+
+const Sra7a = [
+     'صراحه  |  صوتك حلوة؟',
+     'صراحه  |  التقيت الناس مع وجوهين؟',
+     'صراحه  |  شيء وكنت تحقق اللسان؟',
+     'صراحه  |  أنا شخص ضعيف عندما؟',
+     'صراحه  |  هل ترغب في إظهار حبك ومرفق لشخص أو رؤية هذا الضعف؟',
+     'صراحه  |  يدل على أن الكذب مرات تكون ضرورية شي؟',
+     'صراحه  |  أشعر بالوحدة على الرغم من أنني تحيط بك كثيرا؟',
+     'صراحه  |  كيفية الكشف عن من يكمن عليك؟',
+     'صراحه  |  إذا حاول شخص ما أن يكرهه أن يقترب منك ويهتم بك تعطيه فرصة؟',
+     'صراحه  |  أشجع شيء حلو في حياتك؟',
+     'صراحه  |  طريقة جيدة يقنع حتى لو كانت الفكرة خاطئة" توافق؟',
+     'صراحه  |  كيف تتصرف مع من يسيئون فهمك ويأخذ على ذهنه ثم ينتظر أن يرفض؟',
+     'صراحه  |  التغيير العادي عندما يكون الشخص الذي يحبه؟',
+     'صراحه  |  المواقف الصعبة تضعف لك ولا ترفع؟',
+     'صراحه  |  نظرة و يفسد الصداقة؟',
+     'صراحه  |  ‏‏إذا أحد قالك كلام سيء بالغالب وش تكون ردة فعلك؟',
+     'صراحه  |  شخص معك بالحلوه والمُره؟',
+     'صراحه  |  ‏هل تحب إظهار حبك وتعلقك بالشخص أم ترى ذلك ضعف؟',
+     'صراحه  |  تأخذ بكلام اللي ينصحك ولا تسوي اللي تبي؟',
+     'صراحه  |  وش تتمنى الناس تعرف عليك؟',
+     'صراحه  |  ابيع المجرة عشان؟',
+     'صراحه  |  أحيانا احس ان الناس ، كمل؟',
+     'صراحه  |  مع مين ودك تنام اليوم؟',
+     'صراحه  |  صدفة العمر الحلوة هي اني؟',
+     'صراحه  |  الكُره العظيم دايم يجي بعد حُب قوي " تتفق؟',
+     'صراحه  |  صفة تحبها في نفسك؟',
+     'صراحه  |  ‏الفقر فقر العقول ليس الجيوب " ، تتفق؟',
+     'صراحه  |  تصلي صلواتك الخمس كلها؟',
+     'صراحه  |  ‏تجامل أحد على راحتك؟',
+     'صراحه  |  اشجع شيء سويتة بحياتك؟',
+     'صراحه  |  وش ناوي تسوي اليوم؟',
+     'صراحه  |  وش شعورك لما تشوف المطر؟',
+     'صراحه  |  غيرتك هاديه ولا تسوي مشاكل؟',
+     'صراحه  |  ما اكثر شي ندمن عليه؟',
+     'صراحه  |  اي الدول تتمنى ان تزورها؟',
+     'صراحه  |  متى اخر مره بكيت؟',
+     'صراحه  |  تقيم حظك ؟ من عشره؟',
+     'صراحه  |  هل تعتقد ان حظك سيئ؟',
+     'صراحه  |  شـخــص تتمنــي الإنتقــام منـــه؟',
+     'صراحه  |  كلمة تود سماعها كل يوم؟',
+     'صراحه  |  **هل تُتقن عملك أم تشعر بالممل؟',
+     'صراحه  |  هل قمت بانتحال أحد الشخصيات لتكذب على من حولك؟',
+     'صراحه  |  متى آخر مرة قمت بعمل مُشكلة كبيرة وتسببت في خسائر؟',
+     'صراحه  |  ما هو اسوأ خبر سمعته بحياتك؟',
+     '‏صراحه | هل جرحت شخص تحبه من قبل ؟',
+     'صراحه  |  ما هي العادة التي تُحب أن تبتعد عنها؟',
+     '‏صراحه | هل تحب عائلتك ام تكرههم؟',
+     '‏صراحه  |  من هو الشخص الذي يأتي في قلبك بعد الله – سبحانه وتعالى- ورسوله الكريم – صلى الله عليه وسلم؟',
+     '‏صراحه  |  هل خجلت من نفسك من قبل؟',
+     '‏صراحه  |  ما هو ا الحلم  الذي لم تستطيع ان تحققه؟',
+     '‏صراحه  |  ما هو الشخص الذي تحلم به كل ليلة؟',
+     '‏صراحه  |  هل تعرضت إلى موقف مُحرج جعلك تكره صاحبهُ؟',
+	   '‏صراحه  |  هل قمت بالبكاء أمام من تُحب؟',
+     '‏صراحه  |  ماذا تختار حبيبك أم صديقك؟',
+     '‏صراحه  | هل حياتك سعيدة أم حزينة؟',
+     'صراحه  |  ما هي أجمل سنة عشتها بحياتك؟',
+     '‏صراحه  |  ما هو عمرك الحقيقي؟',
+     '‏صراحه  |  ما اكثر شي ندمن عليه؟',
+	 'صراحه  |  ما هي أمنياتك المُستقبلية؟‏',
+     'صراحه | هل قبلت فتاه؟'
+   ]
+   client.on('message', message => {
+ if(message.content.startsWith(prefix + 'صراحه')) {
+  if(!message.channel.guild) return message.reply('** ممنوع كتابة الاوامر في خاص البوت**');
+  var client= new Discord.RichEmbed()
+  .setTitle("لعبة صراحة ..")
+  .setColor('RANDOM')
+  .setDescription(`${Sra7a[Math.floor(Math.random() * Sra7a.length)]}`)
+  .setImage("https://cdn.discordapp.com/attachments/371269161470525444/384103927060234242/125.png")
+                  .setTimestamp()
+
+   message.channel.sendEmbed(client);
+   message.react("??")
+ }
+});
+
+
+
+
+
+   client.on('message', message => {
+    if (message.author.bot) return;
+     if (message.content  === prefix + "help") {
+          const embed = new Discord.RichEmbed()
+  
+
+   .setColor('RANDOM')
+  .setTimestamp()
+
+  .addField(":shield: ***⦁⦓ العاب بوت ⦔⦁*** :shield:",' ‎ ')
+  .addField("✽-  **${prefix}cut  -->  『x اسئله كت تويت x』**",' ‎ ')
+   .addField("✽-  **${prefix}لو خيروك   -->  『x اسئله لو خيروك x』**",' ‎ ')
+     .addField("✽-  **${prefix}تفك -->  『x يعطيك كلمه وتفككها x』**",' ‎ ')
+	   .addField("✽-  **${prefix}اسرع  -->  『x يعطيك كلمه ولازم تكتبها بسرعه x』**",' ‎ ')
+	 	   .addField("✽-  **${prefix}صراحة    -->  『x يعطيك جمله تجاوب عليها بكل صراحه x』**",' ‎ ')
+            
+ .setFooter('||جميع الحقوق محفوضه لدى بترولي ©||')
+
+
+   message.channel.send({embed});
+
+
+    }
+});
+
+
+client.login("NzMyNzA1MDc3MzE5NTY1Mzg2.XxBGiQ.lphJ6T4lcVaBB1PXGk2g4jUtO0U");
